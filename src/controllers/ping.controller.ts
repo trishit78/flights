bb9159dbf1cdf057ae5bcf74e800b7f1a6a951f3
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-12-15 16:28:57 +0530

    basic be folder structure added

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+import logger from "../config/logger.config";
+
+export const pingHandler = async (req: Request, res: Response, next: NextFunction) => {
+    logger.info("Ping request received");
+    res.status(200).json({ message: "Pong!" });
+}
