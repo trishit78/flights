3059c24a9d1abcc7121646011f52771d979dc46e
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-12-23 19:15:46 +0530

    added idempontency key to payment api

--- flights booking service/src/controllers/booking.controller.ts
+++ flights booking service/src/controllers/booking.controller.ts
@@ -11,11 +11,32 @@ export const createBooking =  async(req:Request,res:Response)=>{
     }) 
 }
 
-export const makePayment = async(req:Request,res:Response)=>{
-    const response = await makePaymentService(req.body);
-    res.status(StatusCodes.OK).json({
-        success:true,
-        message:"Payment done successfully",
-        data:response
-    })
-}
+const inMemDb :Record<string,boolean> = {};
+export const makePayment = async (req:Request, res:Response) => {
+  const idempotencyKey = req.headers["x-idempotency-key"];
+
+  if (typeof idempotencyKey !== "string") {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: "idempotency key missing or invalid"
+    });
+    return;
+  }
+
+  if (inMemDb[idempotencyKey]) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: "Cannot retry on a successful payment"
+    });
+    return;
+  }
+
+  const response = await makePaymentService(req.body);
+
+  inMemDb[idempotencyKey] = true;
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Payment done successfully",
+    data: response
+  });
+};
+
