2c2dd720826f7d8834222dbeab91e467c1c63eb3
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-12-24 13:13:14 +0530

    added rate limiter and reverse proxy to different services

--- api gateway/src/config/index.ts
+++ api gateway/src/config/index.ts
@@ -5,6 +5,8 @@ type ServerConfig = {
     PORT: number,
     JWT_SECRET:string,
     JWT_EXPIRY:string
+    FLIGHT_SERVICE:string,
+    BOOKING_SERVICE:string
 }
 
 function loadEnv() {
@@ -17,6 +19,7 @@ loadEnv();
 export const serverConfig: ServerConfig = {
     PORT: Number(process.env.PORT) || 3001,
     JWT_SECRET:process.env.JWT_SECRET || 'trishit',
-    JWT_EXPIRY:process.env.JWT_EXPIRY || '1d'
-
+    JWT_EXPIRY:process.env.JWT_EXPIRY || '1d',
+    FLIGHT_SERVICE:process.env.FLIGHT_SERVICE || 'http://localhost:3000',
+    BOOKING_SERVICE:process.env.BOOKING_SERVICE || 'http://localhost:4000'
 };
--- api gateway/src/index.ts
+++ api gateway/src/index.ts
@@ -1,12 +1,53 @@
 import express from 'express';
 import { serverConfig } from './config/index.js';
 import apiRouter from './routers/index.js';
+import rateLimit from 'express-rate-limit';
+import * as proxy from 'http-proxy-middleware';
+
 
 const app = express();
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 10, // Limit each IP to 10 requests per `window`
+});
 
+
+
+app.use(limiter);
+
+const flightProxyOptions:proxy.Options & { target: string ,changeOrigin:boolean} = {
+  target: serverConfig.FLIGHT_SERVICE,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/flightsService': '/'
+  }
+};
+
+const bookingProxyOptions:proxy.Options & { target: string ,changeOrigin:boolean} = {
+  target: serverConfig.BOOKING_SERVICE,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/bookingService': '/'
+  }
+};
+
+
+app.use(
+  '/bookingService',
+  proxy.createProxyMiddleware(bookingProxyOptions)
+);
+app.use(
+  '/flightsService',
+  proxy.createProxyMiddleware(flightProxyOptions)
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+
+
+
 
 
 app.use('/api',apiRouter);
@@ -15,3 +56,4 @@ app.use('/api',apiRouter);
 app.listen(serverConfig.PORT,()=>{
     console.log(`server is on port ${serverConfig.PORT}`);
 })
+
